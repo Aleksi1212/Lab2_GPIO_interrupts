3df36865f11c04f764ab68b895e398c0478c4935
Aleksi Noro <aleksinoro@gmail.com>
2025-03-26 18:35:16 +0200

    change led level when rotating knob

--- src/main.c
+++ src/main.c
@@ -2,6 +2,62 @@
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
 
-int main(void) {
+#include "../include/constants.h"
+#include "../include/utils.h"
+
+static uint16_t led_level = MAX_LED_LEVEL + 1;
+
+void change_led_level(uint gpio, uint32_t events)
+{
+    int direction = gpio_get(ROT_B);
+    if (direction == 0 && led_level < MAX_LED_LEVEL + 1) {
+        led_level += 50;
+    } else if (direction > 0 && led_level > 0) {
+        led_level -= 50;
+    }
+}
+
+int main(void)
+{
+    uint led_slices[LED_COUNT];
+
+    init_led_pin(LED_1, &led_slices[0]);
+    init_led_pin(LED_2, &led_slices[1]);
+    init_led_pin(LED_3, &led_slices[2]);
+
+    init_rot_pin(ROT_A);
+    init_rot_pin(ROT_B);
+
+    init_sw_pin(ROT_SW);
+
+    stdio_init_all();
+
+    bool pressed = false;
+    bool led_on = true;
+
+    while (true)
+    {
+        if (pressed && gpio_get(ROT_SW) == 0) {
+            pressed = false;
+        }
+        else if (!pressed && gpio_get(ROT_SW) > 0) {
+            if (led_on && led_level == 0)
+                led_level = (MAX_LED_LEVEL + 1) / 2;
+            else
+                led_on = !led_on;
+            
+            pressed = true;
+            sleep_ms(10);
+        }
+
+        if (led_on) {
+            gpio_set_irq_enabled_with_callback(ROT_A, GPIO_IRQ_EDGE_RISE, true, &change_led_level);
+
+            adjust_leds(led_slices, led_level);
+        } else {
+            adjust_leds(led_slices, 0);
+        }
+    }
+
     return 0;
 }
